cc1036650a0f79304842a24244700c4a323a8de7
Nabeelah <nabeelahyousuph@gmail.com>
2019-06-18 13:39:46 +0100

    Create action creators

--- todo/src/components/actions/index.js
+++ todo/src/components/actions/index.js
@@ -1,1 +1,13 @@
+import uuid from 'uuid';
 export const ADD_TODO = 'ADD_TODO';
+
+export function addTodo (todo) {
+    return {
+        type: ADD_TODO,
+        payload: {
+            id: uuid(),
+            todo,
+            done: false
+        }
+    }
+}
--- todo/src/components/reducers/index.js
+++ todo/src/components/reducers/index.js
@@ -1,11 +1,11 @@
-import * as types from "../actions";
+import { ADD_TODO } from "../actions";
 
 export function todoReducer(
   state = [{ todo: "Learn React", done: false }],
   action
 ) {
   switch (action.type) {
-    case types.ADD_TODO:
+    case ADD_TODO:
       return [...state, action.payload];
     default:
       return state;
